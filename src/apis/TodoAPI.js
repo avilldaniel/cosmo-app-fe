b291d606b5dbc3fc4055ab4ba1690cff21927203
avilldaniel <90011911+avilldaniel@users.noreply.github.com>
2022-03-07 13:27:58 -0800

    ready for Vercel deployment

--- src/apis/TodoAPI.js
+++ src/apis/TodoAPI.js
@@ -2,11 +2,5 @@ import axios from 'axios';
 
 // export new instance of axios
 export default axios.create({
-  baseURL: 'http://localhost:4000/api/v1/todos',
+  baseURL: 'https://cosmo-todo-node-api.herokuapp.com/api/v1/todos',
 })
-
-// axios.post(`https://localhost:4000/api/v1/todos`, todo)
-//       .then(res => {
-//         console.log(res);
-//         console.log(res.data);
-//       })
